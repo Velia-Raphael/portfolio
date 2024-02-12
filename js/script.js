2108c2537cdf18d68d2fac744fb209f793ee93ed
Raphael <newraph1@gmail.com>
2024-02-12 23:59:52 +0100

    V1 terminée, responsive non terminé

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*==================== Typing animation ====================*/
+var typed = new Typed(".typing", {
+    strings: ["", "student", ""],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+});
